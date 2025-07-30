46e939fda29db34dd7abe86f19cbbc48f65e71db
Mariano Ceccato <ceccato.mariano@gmail.com>
2025-07-30 11:26:37 +0200

    added ScType with my edited container

--- tools/sctype/scripts/do_solidity.sh
+++ tools/sctype/scripts/do_solidity.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 FILENAME="$1"
 TIMEOUT="$2"
@@ -7,9 +7,30 @@ BIN="$3"
 export PATH="$BIN:$PATH"
 chmod +x "$BIN/solc"
 
-ls $BIN
-pwd
-ls /home/slither/.local/bin/
-which solc
+#chmod o+r "$BIN"/*
+#chmod o+w "$BIN"/*
 
+#chmod o+r "$FILENAME"
+#chmod o+w "$FILENAME"
+
+#echo 1
+#echo $BIN
+#ls -l $BIN
+
+#echo 2
+#pwd
+
+#echo 3
+#ls /home/slither/.local/bin/
+
+#echo 4
+#which solc
+
+#echo 5
+#which slither
+
+#echo 6
+#runuser -u slither -- whoami
+
+#runuser -u slither -- slither --detect tcheck "$FILENAME" --json /output.json
 slither --detect tcheck "$FILENAME" --json /output.json
