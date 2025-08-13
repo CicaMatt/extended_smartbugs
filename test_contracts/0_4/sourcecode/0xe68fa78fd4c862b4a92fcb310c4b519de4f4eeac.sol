1ee35d5cccf3fc54b97a9805323da427171ac70c
Matteo <matteocicalese01@gmail.com>
2025-08-13 16:31:13 +0200

    uploaded contracts for tests

---
+++ test_contracts/0_4/sourcecode/0xe68fa78fd4c862b4a92fcb310c4b519de4f4eeac.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.25;
+
+
+interface IERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+
+contract CONTRACT {
+    function bumpEth(address[] recipients, uint256[] values) external payable {
+        for (uint256 i = 0; i < recipients.length; i++)
+            recipients[i].transfer(values[i]);
+        uint256 balance = address(this).balance;
+        if (balance > 0)
+            msg.sender.transfer(balance);
+    }
+
+
+}
