73f67b804d8c99d8c2428e568cc66cf5f903fb49
Gerardo Iuliano <gerardoiuliano99@gmail.com>
2025-12-18 13:59:32 +0000

    Fixed EtherSolve integration

--- tools/ethersolve/scripts/do_bytecode.sh
+++ tools/ethersolve/scripts/do_bytecode.sh
@@ -4,4 +4,5 @@ EVMFILE="$1"
 
 FILENAME="$(basename $EVMFILE)"
 
-java -jar EtherSolve.jar --re-entrancy --tx-origin -o out/"$FILENAME".json -c -j "$EVMFILE"
+java -jar EtherSolve.jar --re-entrancy --tx-origin -o out/"$FILENAME".json -c -j "$EVMFILE" && 
+for f in /*.csv; do [ -f "$f" ] && mv "$f" /out/; done
